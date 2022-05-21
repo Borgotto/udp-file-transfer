23c984345a52931d410e9a3cf9bb007f18a0b168
Emanuele Borghini <manuborg2002@gmail.com>
2022-05-21 19:43:00 +0200

    Write Client and Server

--- client.py
+++ client.py
@@ -1,1 +1,63 @@
 #!/usr/bin/env python3
+
+import sys
+import socket
+import protocol
+
+# get server ip through command args
+try:
+    address = (sys.argv[1], protocol.SERVER_PORT)
+except IndexError:
+    sys.exit("you need to specify the server ip to send commands to it")
+
+# instantiating UPD socket
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+    sock.settimeout(protocol.TIMEOUT_MAX)
+
+    # starting the client
+    print("client started\ninput commands:")
+    while True:
+        try:
+            # get input from user and convert it to a Command object
+            user_input : str = input().strip()
+            command = protocol.client_commands.get(next(iter(user_input.split()),''))
+            if not command:
+                raise protocol.CommandError("invalid client command")
+
+            # convert the user input to bytes
+            bytes = command.obj_to_bytes(user_input)
+
+            # package bytes and send them to the server
+            packets = protocol.make_packets(bytes)
+            for packet in packets:
+                sock.sendto(packet, address)
+
+            # receive response packets
+            received_packets = []
+            while True:
+                data, ret_addr = sock.recvfrom(protocol.PKT_SIZE)
+                if ret_addr == address: received_packets.append(data)
+                if not data or data.endswith(protocol.MSG_DEL): break
+
+            # unpack the response into bytes
+            received_bytes = protocol.read_packets(received_packets)
+
+            # check the server's response for errors
+            if received_bytes.startswith(protocol.ERROR):
+                raise protocol.CommandError(received_bytes.split(protocol.ERROR,1)[1].decode("utf8"))
+            if not received_bytes.startswith(command.name.encode()):
+                raise protocol.CommandError("invalid response command")
+
+            # do something with the response
+            response_obj = command.obj_from_bytes(received_bytes)
+
+        # raised in case of protocol errors
+        except protocol.CommandError as e:
+            print(e)
+
+        except socket.timeout as e:
+            print("command timed out")
+
+        except KeyboardInterrupt:
+            print("closing client")
+            break
--- server.py
+++ server.py
@@ -1,1 +1,71 @@
 #!/usr/bin/env python3
+
+import socket
+import protocol
+
+clients : "dict[tuple[str, int], list[bytes]]" = {}
+
+# instantiating UPD socket
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+    sock.bind(('', protocol.SERVER_PORT))
+    sock.settimeout(protocol.TIMEOUT_MAX)
+
+    # starting the server
+    print("server started - waiting for commands...")
+    while True:
+        try:
+            # receive the packets
+            address: "tuple[str, int]"
+            while True:
+                data, address = sock.recvfrom(protocol.PKT_SIZE)
+
+
+                if address not in clients:
+                    if len(clients) >= protocol.MAX_CLIENTS:
+                        raise protocol.CommandError("max client connections reached")
+                    clients[address] = [data]
+                else:
+                    clients[address].append(data)
+
+                if not data or data.endswith(protocol.MSG_DEL):
+                    break
+
+            # convert the received packets into readable bytes
+            received_bytes = protocol.read_packets(clients[address])
+
+
+            # get the command from the received bytes
+            command_name = received_bytes.split(protocol.CMD_DEL, 1)[0].decode("utf8")
+            command = protocol.server_commands.get(command_name)
+            if command is None:
+                raise protocol.CommandError('invalid server command')
+
+            # log client command
+            print(address, command_name)
+
+            # convert the received bytes to object
+            received_obj = command.obj_from_bytes(received_bytes)
+
+            # do something with the object and return response bytes
+            bytes = command.obj_to_bytes(received_obj)
+
+            # send all of the response divided in packets of PKT_SIZE bytes
+            packets = protocol.make_packets(bytes)
+            for packet in packets:
+                sock.sendto(packet, address)
+
+        # raised in case of protocol errors
+        except protocol.CommandError as e:
+            sock.sendto(e.bytes, address)
+
+        except socket.timeout as e:
+            pass
+
+        except Exception as e:
+            sock.sendto(protocol.CommandError("the server encountered and error: "+str(e)).bytes, address)
+
+        finally:
+            try:
+                del clients[address] # remove client from list of peers
+            except:
+                pass
